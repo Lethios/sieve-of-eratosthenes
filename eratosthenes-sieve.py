f51022c9834925417bfece9f306bd8a22ec3712b
Lethios <204111230+Lethios@users.noreply.github.com>
2025-06-14 15:47:46 +0530

    Update eratosthenes-sieve.py

--- eratosthenes-sieve.py
+++ eratosthenes-sieve.py
@@ -1,1 +1,35 @@
+import sys
 
+HELP_MESSAGE = f"""
+Usage: python {sys.argv[0]} <command>
+
+Available commands:
+    help                 - Displays this help message.
+    <positive_integer>   - The upper bound upto which prime numbers are generated.
+"""
+
+if sys.argv[1] == "help":
+    print(HELP_MESSAGE)
+    sys.exit(0)
+
+elif sys.argv[1].isdigit():
+    limit = int(sys.argv[1])
+
+    prime_bool_list = [True] * (limit + 1)
+    prime_list = []
+
+    prime_bool_list[0], prime_bool_list[1] = False, False
+
+    for number, prime in enumerate(prime_bool_list):
+        if prime:
+            prime_list.append(number)
+
+            for i in range(number * 2, limit + 1, number):
+                prime_bool_list[i] = False
+
+    print(prime_list)
+    sys.exit(0)
+
+else:
+    print("Enter a valid integer.")
+    sys.exit(1)
